183116aee14e128cf029c6e8180cf9507ad4e952
Velasxo <106454325+Velasxo@users.noreply.github.com>
2022-05-29 07:13:53 +0800

    Add files via upload

---
+++ OnlineBankSystem.java
@@ -0,0 +1,121 @@
+package client;
+
+import java.sql.ClientInfoStatus;
+import java.util.Scanner;
+
+
+public class OnlineBankSystem {
+
+  public static void main(String[] args) {
+  
+  Scanner = new Scanner(System.in);
+  String = "Yes";
+  
+  Client client = Client.getclient();
+  
+  do{
+    
+    int loanId = 100;
+    int savingId = 50;
+    
+    bankadviser bankadviser = null;
+    
+    system.out.println("log in your username:john and password:44321 status: alive credentials: position:loaner client");
+    
+    String ans =Scanner.nextLine();
+    if(ans.equalIgnorecase("Yes")){
+      
+      Accounts = BankAgreement();
+      
+     System.out.println("your account deatails are secured");
+     
+     
+     String Client= Scanner.nextLine();
+     
+     Client.setClientName(ClientName);
+     
+     Client.addNewloan(loan);
+     
+     if(Client.update()==false){
+       Client.verification();
+       
+       Client.add();
+       
+       processrequest();
+       
+       System.out.println("your current savings is 50");
+       
+       System.out.println("and your current load is 100");
+       
+       int_money_count = Scanner.nextInt();
+       
+       if(i==money_count -100){
+         
+         Scanner.nextLine();
+       }
+       
+       Bank Adviser = Client.BankAdviser ();
+       put("account");
+       
+       Client.giveSavings(money);
+     }
+    } 
+    
+    else{
+      
+      System.out.println("Heres your Cash ");
+      
+      System.out.println("I received Immediately the 100");
+      
+      System.out.println("total of your loan is 100");
+      
+      Client.BankAdviser().Accounts();
+      
+      System.out.println("please Verify your account");
+      
+      loanId = integer.parseInt(Scanner.nextLine());
+      if(loanId = 100){
+        
+        Client
+        (loanId);
+        Loan
+        (loanId);
+        
+        
+      }
+    }
+  }
+      else{
+        
+        if(Client.BankAgreement()==true){
+          
+          System.out.println("your current balance is 100");
+          
+          System.out.ptintln("your saving is 50");
+          
+          inumberTable = Scanner.nextInt();
+          
+          Scanner.nextLine();
+          
+          if(Client != 100){
+            
+            Client.
+            Receive (Client);
+            Client.
+            Release (Client);
+            
+            System.out.print("our transaction is ended");
+            
+             Scanner.nextLine();
+            
+            if(s.equalsIgnoreCase("No")){
+              
+              System.exit();
+            }
+          }
+        } while (s.equalsIgnoreCase("Yes"));
+      }
+      
+  }
+
+}
